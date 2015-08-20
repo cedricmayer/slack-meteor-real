1299ec7febd9a008922b8d80a613376302753b09
cedricmayer <jhm2157@columbia.edu>
2015-08-20 11:44:00 -0500

    post button working

---
+++ client/lib/router.js
@@ -0,0 +1,28 @@
+
+Router.route('/', {
+	template: 'home',
+	waitOn: function(){
+        return [IRLibLoader.load('http://cdn.jsdelivr.net/jquery.mixitup/latest/jquery.mixitup.min.js')]
+    }
+});
+
+Router.route('/form');
+
+Router.configure({
+    layoutTemplate: 'main'
+});
+
+
+Template.posts.events({												// create new object in database when Submit a new job post form
+	'click .post_button' : function(event) {
+	 	
+		console.log('router should be working');
+	}
+});
+
+
+// Router.map( function () {
+//   this.route('codeEditor',{
+//     waitOn: IRLibLoader.load('http://cdn.jsdelivr.net/jquery.mixitup/latest/jquery.mixitup.min.js')
+//   });
+// });
